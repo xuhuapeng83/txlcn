e0cd5e0a971b66bce2d53ea82796f43c97b93b3b
hpxu83 <xuhuapeng83@sina.com>
2019-12-20 17:37:48 +0800

    update test

--- src/main/java/cn/xhp/ServiceAApplication.java
+++ src/main/java/cn/xhp/ServiceAApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class ServiceAApplication {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(ServiceAApplication.class, args);
     }
